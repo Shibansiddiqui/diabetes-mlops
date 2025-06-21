b39cf8fb5577fd7f944ab817a480a28f79e20525
Shiban Siddiqui <shibansiddiqui@gmail.com>
2025-06-22 03:57:21 +0530

    Add basic test cases

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,18 +1,14 @@
-# test/test_train.py
-
-import sys
+from src.preprocess import load_data, split_data
 import os
 
-# Add the root folder (i.e., "ml ops") to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from src.preprocess import load_data, split_data  # ✅ Correct import path
 def test_load_data():
-    df = load_data(path=r"C:\Users\Lenovo\Desktop\projects\ml ops\data\diabetes.csv")
+    path = os.path.join("data", "diabetes.csv")
+    df = load_data(path)
     assert df.shape[0] > 0
     assert "Outcome" in df.columns
 
 def test_split_data():
-    df = load_data(path=r"C:\Users\Lenovo\Desktop\projects\ml ops\data\diabetes.csv")
+    path = os.path.join("data", "diabetes.csv")
+    df = load_data(path)
     X_train, X_test, y_train, y_test = split_data(df)
     assert len(X_train) > 0 and len(X_test) > 0
